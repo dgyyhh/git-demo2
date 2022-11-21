052b1ae0a53d53d8d18c193c38841aba489e5d81
sunzhikai <sunzhikai2001@163.com>
2022-11-21 16:13:46 +0800

    git demo2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,5 +6,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
     }
 }
